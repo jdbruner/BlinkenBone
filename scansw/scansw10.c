bab812f783b700b37e4fa679262d2b7cf939a659
John D. Bruner <jdbruner@live.com>
2026-03-03 18:52:32 -0800

    add formatted output to scansw10

--- scansw/scansw10.c
+++ scansw/scansw10.c
@@ -6,11 +6,14 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/param.h>
 #include <unistd.h>
 #include <fcntl.h> 
 #include <time.h>				//Needed for nanosleep
 #include <pthread.h>			//Needed for pthread
+#include <stdint.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
@@ -53,11 +56,65 @@ u_int8_t xIO = 22;			// GPIO 22: 0=drive LEDs, 1=read switches
 u_int8_t cols[18] = { 21,20,16,12,7,8,25,24,23,  18,10,9,11,5,6,13,19,26 };
 
 
-int	main(void)
+int
+main(int argc, const char *const *argv)
 {
+    const char *argv0 = (argc > 0) ? argv[0] : "scansw10";
+    char radix = 'u';
+    int width = 0;
+    int zerofill = 0;
+    uint64_t bitmask = (1uL << 36) - 1;
+    char format[16];
 	int	col, row, i, switchscan, tmp;
-    	int num_gpios, ret;
+    int num_gpios, ret;
+	uint64_t result;
+	
+    while (1) {
+        /* parse arguments */
+        int c = getopt(argc, (char **)argv, "0d::o::x::n:?");
+        if (c == -1)
+            break;
 
+        switch (c) {
+        case '0':
+            /* normally used with nonzero width, left fill with zeros */
+            zerofill = 1;
+            break;
+
+        case 'd':
+        case 'u':
+        case 'o':
+        case 'x':
+            /* the numeric radix formatter (decimal, octal, hexadecimal) */
+            radix = (c == 'd') ? 'u' : c;
+            if (optarg != NULL) {
+                width = atoi(optarg);
+                width = MAX(0, MIN(width,12));
+            }
+            break;
+        
+        case 'n':
+            /* number of significant bits, starting from the low end */
+            if (optarg != NULL) {
+                int nbits = atoi(optarg);
+                if (nbits <= 0 || nbits >36)
+                    nbits = 36;
+                bitmask = (1uL << nbits) - 1;
+            }
+            break;
+
+        default:
+            fprintf(stderr, "%s: unknown argument \"%c\"\n", argv0, c);
+            /* fall through */
+        case '?':
+            fprintf(stderr, "Usage: \"%s [-0] [-d[N]|-o[N]|-x[N]] [-nN]\"\n", argv0);
+            return 1;
+        }
+    }
+
+    /* create the format string for the final output */
+    snprintf(format, sizeof format, "%%%s%dl%c\n", zerofill ? "0" : "", width, radix);
+	
 	// init GPIO ----------
 	ret = gpiolib_init();
 	if (ret < 0)
@@ -108,6 +165,7 @@ int	main(void)
 	for (i = 0; i < 18; i++) 
 		gpio_set_dir(cols[i], DIR_INPUT);
 
+
 	for (row=0; row<5; row++)		// there are 5 rows of 18 switches each
 	{
 		// ----- select row from address
@@ -129,6 +187,12 @@ int	main(void)
 		}
 		gpio_switchstatus[row] = switchscan;
 	}
-	//printf("\nscansw10 %d\n", 262143-gpio_switchstatus[0]);
-	return 262143-gpio_switchstatus[0];
+
+	result = ~(((uint64_t)gpio_switchstatus[1] << 18) |
+				(gpio_switchstatus[0] & 0777777)) & bitmask;
+
+	// print the switch register according to the format determined above
+	printf(format, result);
+
+	return result & 0177;
 }
