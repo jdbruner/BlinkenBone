ed919df5956eaa1dbdbc1629e01e7fc9ec010557
John D. Bruner <jdbruner@live.com>
2026-07-23 11:48:23 -0700

    add get-selections.ps1 and add column output to get_selections.sh

--- scripts/get_selections.sh
+++ scripts/get_selections.sh
@@ -112,7 +112,7 @@ if [[ -d "${basedir}" ]] ; then
                 if [[ -z "${opt_v}" ]]; then
                     printf '%s\t' ${values[csw]}
                     if [[ -n "${values[desc]}" ]]; then
-                        printf '%s\n' "${values[desc]}"
+                        printf '%s: %s\n' "${values[dir]}" "${values[desc]}"
                     else
                         printf '%s\n' "${values[dir]}"
                     fi
@@ -126,5 +126,10 @@ if [[ -d "${basedir}" ]] ; then
                 fi
             fi
         fi
-    done | sort
+    done | sort | \
+        if [[ -z "${opt_v}" ]] ; then
+            column -t -s '	' -o '  ' | column
+        else
+            cat
+        fi
 fi
